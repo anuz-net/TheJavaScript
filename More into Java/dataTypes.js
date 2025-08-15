fc0ceac61e217488ab5532573102fb363b21c17b
Anuz <95576641+anuz-net@users.noreply.github.com>
2025-08-15 07:51:50 +0545

    Add examples for JS data types and object usage
    Introduced two new files: one demonstrating JavaScript primitive and non-primitive data types with object manipulation, and another providing a sample product object with properties and logging.

---
+++ More into Java/dataTypes.js
@@ -0,0 +1,24 @@
+// There are different dataTypes in JS :
+// Number, String, Boolean, undefined, BigInt, Symbol
+
+let x = BigInt(123);
+let y = Symbol(Hello);
+
+
+// Non Primitive DataType - Object
+ const Object = {
+    fullName : "Raju Mistri",
+    age : 20,
+    cgpa : 8.2,
+    isPass: true,
+ };
+
+ //printing object values.
+
+ console.log(student["fullName"]);
+ console.log(student.age);
+ 
+ // Updating the value inside object....
+ student["age"] = student["age"] + 1;
+ console.log(student.age);
+
---
+++ More into Java/practice1.js
@@ -0,0 +1,9 @@
+const productInfo = {
+    productName : "Parker Jotter Standard CT Ball Pen",
+    rating : 4,
+    isDeal : true,
+    price : 240,
+    discount : 5,
+};
+
+console.log(productInfo);
