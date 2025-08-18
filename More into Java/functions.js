4a1d61a1dd08f762547f177ce88cd1274c17337d
Anuz <95576641+anuz-net@users.noreply.github.com>
2025-08-18 07:48:46 +0545

    Add examples of functions and arrow functions in JS
    Added new files demonstrating user-defined functions, arrow functions, and sum functions in JavaScript. Updated functions.js with examples of function declarations, calls, and parameter usage. Introduced arrow-fnxn.js and sumfxn.js to illustrate different function syntaxes and use cases.

--- More into Java/functions.js
+++ More into Java/functions.js
@@ -2,3 +2,24 @@ console.log("hello")
 "abc".toUpperCase();
 
 [1,2,3].push(4);
+
+
+//-------User Defined Function---------------
+
+function myfunction(){
+    console.log("Welcome to Insonomia");
+    console.log("We are Learning JS")
+}
+
+// ----------Calling Function-----------------
+myfunction();
+
+
+
+
+function myfxn(msg) {
+    console.log(msg);
+    
+}
+
+myfxn("ILoveJS",100);
